041bafbf7b35996843d2975ac2657075a6262ace
elvece <lvcifferello@gmail.com>
2015-07-29 20:36:13 -0600

    slight style edit

--- js/dom.js
+++ js/dom.js
@@ -1,39 +1,39 @@
 //Prices DOM manipulation
-// var options = document.getElementsByTagName('select')[0].children;
+var options = document.getElementsByTagName('select')[0].children;
 
-// var selected = document.getElementsByTagName('select')[0].selectedOptions;
+var selected = document.getElementsByTagName('select')[0].selectedOptions;
 
-// var selectedArray = [];
+var selectedArray = [];
 
-// document.getElementById("sum-all").addEventListener("click", function(){
-//   var result = sum(options);
-//   writeAnswer(result);
-//   }
-// );
+document.getElementById("sum-all").addEventListener("click", function(){
+  var result = sum(options);
+  writeAnswer(result);
+  }
+);
 
-// document.getElementById("count-selected").addEventListener("click", function(){
-//   var result = count(selected);
-//   countAnswer(result);
-//   }
-// );
+document.getElementById("count-selected").addEventListener("click", function(){
+  var result = count(selected);
+  countAnswer(result);
+  }
+);
 
-// document.getElementById("sum-selected").addEventListener("click", function(){
-//   var result = sum(selected);
-//   writeAnswer(result);
-//   }
-// );
+document.getElementById("sum-selected").addEventListener("click", function(){
+  var result = sum(selected);
+  writeAnswer(result);
+  }
+);
 
-// document.getElementById("average-all").addEventListener("click", function(){
-//   var result = avg(options);
-//   writeAnswer(result);
-//   }
-// );
+document.getElementById("average-all").addEventListener("click", function(){
+  var result = avg(options);
+  writeAnswer(result);
+  }
+);
 
-// document.getElementById("average-selected").addEventListener("click", function(){
-//   var result = avg(selected);
-//   writeAnswer(result);
-//   }
-// );
+document.getElementById("average-selected").addEventListener("click", function(){
+  var result = avg(selected);
+  writeAnswer(result);
+  }
+);
 
 //Names DOM manipulation
 var names = document.getElementsByTagName("textarea")[0].value;
